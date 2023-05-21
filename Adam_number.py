c85aef99c0db0534489ac4a903d7ae3013859001
Ayyappa19 <119914633+Ayyappa19@users.noreply.github.com>
2023-05-21 20:06:06 +0530

    n=int(input()) m=n*n t=n s=0 while(t!=0):     s=s*10+t%10     t=t//10 m1=s*s s1=0 while(m1!=0):     s1=s1*10+m1%10     m1=m1//10 print(m==s1)

--- Adam_number.py
+++ Adam_number.py
@@ -1,17 +1,13 @@
-def rev(n):
-    p=0
-    while(n):
-        r=n%10
-        p=p*10+r
-        n=n//10
-    return p
-    
 n=int(input())
-a=rev(n)
-x=n*n
-y=a*a
-if(x==rev(y)):
-    print('True')
-else:
-    print('False')
-
+m=n*n
+t=n
+s=0
+while(t!=0):
+    s=s*10+t%10
+    t=t//10
+m1=s*s
+s1=0
+while(m1!=0):
+    s1=s1*10+m1%10
+    m1=m1//10
+print(m==s1)

9637772bce382008cca69d9ed850ccc34c1f2993
Ayyappa19 <119914633+Ayyappa19@users.noreply.github.com>
2023-05-21 16:53:29 +0530

    n=input() l=len(n) k=1 s=0 for i in n:     for j in range(k,l+1):         f=(int(i))**k         k+=1         break     s=s+f # print(s) if(int(n)==s):     print('True') else:     print('False')

---
+++ Disarium_number.py
@@ -0,0 +1,18 @@
+n=input()
+l=len(n)
+k=1
+s=0
+for i in n:
+    for j in range(k,l+1):
+        f=(int(i))**k
+        k+=1
+        break
+    s=s+f
+# print(s)
+if(int(n)==s):
+    print('True')
+else:
+    print('False')
+    
+        
+    
